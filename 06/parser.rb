df4318f183a11fca75d3e58bb709253e983411a9
Ryan Reed <rwreed@smu.edu>
2015-12-09 17:45:04 -0600

    work done on code class

--- 06/parser.rb
+++ 06/parser.rb
@@ -6,8 +6,33 @@ class Parser
 	end
 
 	def parse
-		
+		@assembly_instructions.each do |instruciton|
+			if command_type(instruciton) == :a_command
+				@machine_instructions << assemble_a_command(instruciton)
+			elsif command_type(instruciton) == :c_command
+				@machine_instructions << assemble_c_command(instruciton)
+			end
+		end
 		@machine_instructions
 	end
 
+	def assemble_a_command(instruciton)
+		command = '0'
+		command << constant(instruction[1..-1]
+	end
+
+	def constant(value)
+		"%015b" % value
+	end
+	def assemble_c_command(instruciton)
+		command = "111"
+	end
+
+	def command_type(instruciton)
+		if instruciton.start_with?("@")
+			:a_command
+		else
+			:c_command
+		end
+	end
 end
